723db0d31b23f431f9b97aa9aa21314557a574bf
Tyler <33147253+Neopect@users.noreply.github.com>
2020-06-25 11:29:37 -0500

    Basic Panel cdisplay system

--- Tigris Auxilium/src/ta/views/Tigris_Auxilium.java
+++ Tigris Auxilium/src/ta/views/Tigris_Auxilium.java
@@ -3,10 +3,15 @@ package ta.views;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.Toolkit;
 
 public class Tigris_Auxilium {
 
-	private JFrame frame;
+	private JFrame frmTigrisAuxilium;
 
 	/**
 	 * Launch the application.
@@ -16,7 +21,7 @@ public class Tigris_Auxilium {
 			public void run() {
 				try {
 					Tigris_Auxilium window = new Tigris_Auxilium();
-					window.frame.setVisible(true);
+					window.frmTigrisAuxilium.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +40,40 @@ public class Tigris_Auxilium {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmTigrisAuxilium = new JFrame();
+		frmTigrisAuxilium.setIconImage(Toolkit.getDefaultToolkit().getImage(Tigris_Auxilium.class.getResource("/ta/resources/NPN Logo.png")));
+		frmTigrisAuxilium.setTitle("Tigris Auxilium");
+		frmTigrisAuxilium.setBounds(100, 100, 592, 408);
+		frmTigrisAuxilium.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmTigrisAuxilium.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelMenu = new JPanel();
+		frmTigrisAuxilium.getContentPane().add(panelMenu, BorderLayout.NORTH);
+		panelMenu.setLayout(new GridLayout(0, 7, 0, 0));
+		
+		JButton btnNewButton = new JButton("New button");
+		panelMenu.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		panelMenu.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		panelMenu.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		panelMenu.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("New button");
+		panelMenu.add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("New button");
+		panelMenu.add(btnNewButton_5);
+		
+		JButton btnNewButton_6 = new JButton("New button");
+		panelMenu.add(btnNewButton_6);
+		
+		PanelAuxy panelMain = new PanelAuxy();
+		frmTigrisAuxilium.getContentPane().add(panelMain, BorderLayout.CENTER);
 	}
 
 }
